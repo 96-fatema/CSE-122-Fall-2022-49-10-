fa9d4a935b94b8365b2f1f6922e68bafe21f9baa
Fatema tuz zohora <103174162+96-fatema@users.noreply.github.com>
2022-07-20 21:59:53 +0600

    Add files via upload

---
+++ Lab 01/watermelon.cpp
@@ -0,0 +1,15 @@
+//codeforces problem no:4A
+//problem name: watermelon
+
+#include <iostream>
+using namespace std;
+int main(){
+int w;
+cin>>w;
+if(w%2==0&&w!=2&&w>0)
+    cout<<"Yes"<<endl;
+else
+    cout<<"No"<<endl;
+return 0;
+
+}
