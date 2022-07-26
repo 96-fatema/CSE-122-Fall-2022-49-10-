7287737dbc70d24a38bba96cb10b7b705ed30533
Fatema tuz zohora <103174162+96-fatema@users.noreply.github.com>
2022-07-26 23:08:50 +0600

    Add files via upload

---
+++ Lab 02/dominopiling.cpp
@@ -0,0 +1,11 @@
+//codeforces problem no.50A
+//codeforces problem name:Dominopiling
+#include <iostream>
+using namespace std;
+int main()
+{
+    int M,N;
+    cin>>M>>N;
+    cout<<M*N/2<<endl;
+    return 0;
+}
